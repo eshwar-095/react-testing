2c9e8ef4fe10b16c6c8782b88236fd528bf4cf64
eshwar-095 <eshwar095@gmail.com>
2023-08-01 12:57:23 +0530

    RTL Queries

---
+++ src/components/Application/Application.test.tsx
@@ -0,0 +1,70 @@
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
+
+describe("Application", () => {
+  test("renders correctly", () => {
+    render(<Application />);
+
+    /** getByRole */
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
+    expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox");
+    expect(termsElement).toBeInTheDocument();
+
+    const submitBUttonElement = screen.getByRole("button");
+    expect(submitBUttonElement).toBeInTheDocument();
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    /** getByLabelText */
+    const nameElement2 = screen.getByLabelText("Name", {
+      selector: "input",
+    });
+    expect(nameElement2).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
+
+    /** getByPlaceholderText */
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    /** getByText */
+    const paraElement = screen.getByText("All fields are mandatory");
+    expect(paraElement).toBeInTheDocument();
+
+    /** getByAltText */
+    const imageElement = screen.getByAltText("a person with a laptop");
+    expect(imageElement).toBeInTheDocument();
+
+    /** getByTitle */
+    const spanElement = screen.getByTitle("close");
+    expect(spanElement).toBeInTheDocument();
+
+    /** getByTestId */
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+  });
+});
